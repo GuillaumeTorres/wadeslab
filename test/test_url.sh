70d531fd13a701998555c312bc8b0a43988b7a87
Louis Delfieux <louis.delfieux@alterway.fr>
2018-03-24 19:43:34 +0100

    add minimal url check for jenkins build validation

---
+++ test/test_url.sh
@@ -0,0 +1,16 @@
+for url in {jenkins.,,www.}wadeslab.tk
+do
+    response=$( curl -k -s -o /dev/null -w "%{http_code}" https://$url )
+
+    if [ "$response" != "200" ]
+    then
+        if [ "$url" = "jenkins.wadeslab.tk" ]
+        then
+            if [ "$response" = "403" ]
+            then
+                continue
+            fi
+        fi
+        exit 1
+    fi
+done 
